15147ec2ae90439856250e513c282a660e0d431c
Victor Weng <victor.weng@gmail.com>
2017-10-24 17:46:37 -0400

    updated shell scripts to use GitHub token as parameter

--- cot-labels.sh
+++ cot-labels.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 #
-# This shell should be run from the git project folder that sits in sibling with the git-label-packages.
-# The git project folder name is expected to be the same as git repo name, otherwise won't work
 # The script requires install of https://github.com/jasonbellamy/git-label-cli first, and doesn't support proxy.
 # 
-git-label -a https://api.github.com -r CityofToronto/${PWD##*/} -t 23b24e4db84cd6da05fee044997ecc85d28c932e -p ../git-label-packages/packages/default.json remove
-git-label -a https://api.github.com -r CityofToronto/${PWD##*/} -t 23b24e4db84cd6da05fee044997ecc85d28c932e -p ../git-label-packages/packages/status.json add
-git-label -a https://api.github.com -r CityofToronto/${PWD##*/} -t 23b24e4db84cd6da05fee044997ecc85d28c932e -p ../git-label-packages/packages/type.json add
-git-label -a https://api.github.com -r CityofToronto/${PWD##*/} -t 23b24e4db84cd6da05fee044997ecc85d28c932e -p ../git-label-packages/packages/priority.json add
+echo "Usage: cot-labels.sh <github-token> <github-repo>"
+if [ "$1" == "" ]; then
+    echo "Missing GitHub Token"
+    exit 1
+fi
+TOKEN=$1
+if [ "$2" == "" ]; then
+    echo "Missing repo name for CityofToronto/<repo-name>"
+    exit 2
+fi
+REPO=$2
+git-label -a https://api.github.com -r CityofToronto/$REPO -t $TOKEN -p packages/default.json remove
+git-label -a https://api.github.com -r CityofToronto/$REPO -t $TOKEN -p packages/status.json add
+git-label -a https://api.github.com -r CityofToronto/$REPO -t $TOKEN -p packages/type.json add
+git-label -a https://api.github.com -r CityofToronto/$REPO -t $TOKEN -p packages/priority.json add
